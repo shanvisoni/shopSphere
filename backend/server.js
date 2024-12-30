be1374557bdd325b65c2879a964911591994b793
shanvisoni <shanvisoni7388@gmail.com>
2024-12-31 01:26:36 +0530

    server.js production correction

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const _dirname=path.resolve();
 app.use(cors({
     origin: [
         "http://localhost:3000",  // Frontend in development
-      
+      "https://shopsphere-2.onrender.com"
       ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
